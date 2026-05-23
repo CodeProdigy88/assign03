2913f94c780aefc336a4eaf7d0a2590b55fe1077
CodeProdigy88 <cameronmckay88@gmail.com>
2026-05-22 21:39:08 -0600

    Add files via upload

---
+++ ArraySet.java
@@ -0,0 +1,64 @@
+package assign03;
+
+import java.util.Collection;
+import java.util.Comparator;
+
+public class ArraySet<E> implements Set<E> {
+
+	public ArraySet() {
+		// TODO
+	}
+
+	public ArraySet(Comparator<? super E> cmp) {
+		// TODO
+	}
+
+	@Override
+	public void add(Object item) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object remove(Object item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean contains(Object item) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsAll(Collection items) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
